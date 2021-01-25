cd318620ec258a5878ac5ca381a300e61c3e8a38
joaopaul167 <joaopaulo167@gmail.com>
2021-01-25 16:25:39 -0300

    first commit

---
+++ Assets/Script/RotateSkybox.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition; 
+using UnityEngine.Rendering;
+public class App: MonoBehaviour {
+
+ [SerializeField]
+ Volume volume;
+ 
+ [SerializeField]
+ HDRISky sky;
+ 
+ float skyRotationAmountPerFrame = 0.1f;
+ 
+ void Awake()
+ {
+      volume.profile.TryGet(out sky);
+ }
+ 
+ void Update()
+ {
+        sky.rotation.value = Mathf.Lerp(sky.rotation.value, sky.rotation.value + 10, skyRotationAmountPerFrame * Time.deltaTime);
+        if (sky.rotation.value == sky.rotation.max)
+        {
+            sky.rotation.value = 0;
+        }
+ }
+}
